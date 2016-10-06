13bc759f69fc59ad04eb20f4ae9ac8ee95687c27
leandrocgsi <leandrocgsi@gmail.com>
2016-10-05 21:05:11 -0300

    Starting code

---
+++ src/main/java/br/com/uol/vericad/gson/PrettyPrintJSON.java
@@ -0,0 +1,17 @@
+package br.com.uol.vericad.gson;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+public class PrettyPrintJSON {
+	
+	public String toPrettyFormat(String jsonString) {
+		JsonParser parser = new JsonParser();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		
+		JsonElement el = parser.parse(jsonString);
+		return gson.toJson(el);
+	}
+}
