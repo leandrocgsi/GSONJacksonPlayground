7ae497f66098ade588085ddc823c3ae0436252e2
leandrocgsi <leandrocgsi@gmail.com>
2016-10-07 21:06:24 -0300

    Starting code

---
+++ src/main/java/br/com/uol/vericad/jackson/JSONNodeToArrayNode.java
@@ -0,0 +1,28 @@
+package br.com.uol.vericad.jackson;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class JSONNodeToArrayNode {
+	
+	public void jsonNodeToArrayNode(String jsonString) {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode arrNode = null;
+		try {
+			arrNode = objectMapper.readTree(jsonString).get("objects");
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (arrNode.isArray()) {
+		    for (final JsonNode objNode : arrNode) {
+		        System.out.println(objNode);
+		    }
+		}
+	}
+}
