3a90e7f81ca800d60d1ea390d3ff1dcbb06f0951
leandrocgsi <leandrocgsi@gmail.com>
2016-10-08 21:07:05 -0300

    Starting code

---
+++ src/main/java/br/com/uol/vericad/jackson/JSONToHashMapList.java
@@ -0,0 +1,29 @@
+package br.com.uol.vericad.jackson;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.type.TypeFactory;
+
+public class JSONToHashMapList {
+	
+	public List<HashMap<String, Object>> jsonToObjectList(String jsonString) {
+		ObjectMapper mapper = new ObjectMapper();
+		List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+		try {
+			list = mapper.readValue(jsonString, TypeFactory.collectionType(List.class, HashMap.class));
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+}
