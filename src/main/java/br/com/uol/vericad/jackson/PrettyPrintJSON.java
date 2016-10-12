305d450b428a7ccd4026d3b5cc7c44c0ab033c89
leandrocgsi <leandrocgsi@gmail.com>
2016-10-11 21:08:31 -0300

    Starting code

---
+++ src/main/java/br/com/uol/vericad/jackson/PrettyPrintJSON.java
@@ -0,0 +1,28 @@
+package br.com.uol.vericad.jackson;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig.Feature;
+
+public class PrettyPrintJSON {
+	
+	public String toPrettyFormat(String jsonString) {
+		ObjectMapper mapper = new ObjectMapper();
+		String formatedJSON = "";
+		try {
+			Object json = mapper.readValue(jsonString, Object.class);
+			mapper.configure(Feature.INDENT_OUTPUT, true);
+			formatedJSON = mapper.writeValueAsString(json);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return formatedJSON;
+	}
+}
