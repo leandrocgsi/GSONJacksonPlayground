2ad9ab5ff73a682193298d138184574eb39fb642
leandrocgsi <leandrocgsi@gmail.com>
2016-10-17 21:11:39 -0200

    Starting code

---
+++ src/test/java/br/com/uol/vericad/gson/JSONToObjectListTest.java
@@ -0,0 +1,21 @@
+package br.com.uol.vericad.gson;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import br.com.uol.vericad.pojo.User;
+
+public class JSONToObjectListTest {
+
+	private JSONToObjectList jsonToObjectList = new JSONToObjectList();
+
+	@Test
+	public void test() {
+		String jsonString = "[{\"age\":27,\"name\":\"Flávio\"},{\"age\":31,\"name\":\"Leandro\"}]";
+		List<User> list = jsonToObjectList.jsonToObjectList(jsonString);
+		System.out.println(list.get(0).getName() + " - " + list.get(0).getAge());
+		System.out.println(list.get(1).getName() + " - " + list.get(1).getAge());
+	}
+
+}
