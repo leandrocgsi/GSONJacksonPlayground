1477acc1a84b16329ff464f6458aa490ab004eda
leandrocgsi <leandrocgsi@gmail.com>
2016-10-18 21:12:06 -0200

    Starting code

---
+++ src/test/java/br/com/uol/vericad/gson/PrettyPrintJSONTest.java
@@ -0,0 +1,14 @@
+package br.com.uol.vericad.gson;
+
+import org.junit.Test;
+
+public class PrettyPrintJSONTest {
+	
+	private PrettyPrintJSON prettyPrintJSON = new PrettyPrintJSON();
+
+	@Test
+	public void test() {
+		String jsonString = "{\"age\":29,\"name\":\"mkyong\"}";
+		System.out.println(prettyPrintJSON.toPrettyFormat(jsonString));;
+	}
+}
